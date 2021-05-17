5ec73936b448e68f4bf0afbc342f94ae98f27711
MagentaSpruce <magentaspruce@protonmail.com>
2021-05-17 14:33:02 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+class Workout {
+  date = new Date();
+  id = Date.now() + ''.slice(-10);
+  clicks = 0;
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+
+//   _setDescription() {
+//     // prettier-ignore
+//     const months = [
+//       'January',
+//       'February',
+//       'March',
+//       'April',
+//       'May',
+//       'June',
+//       'July',
+//       'August',
+//       'September',
+//       'October',
+//       'November',
+//       'December',
+//     ]};
